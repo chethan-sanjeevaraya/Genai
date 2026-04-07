353d1154c54cc7d0cb4151a6f7b386e77bccac64
chethanmaris <chethan.maris@gmail.com>
2026-04-08 00:11:00 +0530

    folder str created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_Bot_Project',
+    version= '0.0.0',
+    author= 'Chethan',
+    author_email= 'chethan.maris@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
